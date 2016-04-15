c2ebbe560afc302f0c17ae865c8089efe6b33386
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2016-04-15 13:33:24 +0200

    Remove final modifier from static method

--- src/main/java/nl/knaw/huygens/pergamon/server/Pergamon.java
+++ src/main/java/nl/knaw/huygens/pergamon/server/Pergamon.java
@@ -1,7 +1,7 @@
 package nl.knaw.huygens.pergamon.server;
 
 public class Pergamon {
-  public static final void main(String... args) {
+  public static void main(String... args) {
     System.err.println("Hello world!");
   }
 }
