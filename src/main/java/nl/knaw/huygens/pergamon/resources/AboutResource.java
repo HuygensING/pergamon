c64e17d0fe807fba1a5fd21448bc30f77b21b9f8
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2016-04-16 21:25:30 +0200

    Makes /about yield json result

--- src/main/java/nl/knaw/huygens/pergamon/resources/AboutResource.java
+++ src/main/java/nl/knaw/huygens/pergamon/resources/AboutResource.java
@@ -2,12 +2,27 @@ package nl.knaw.huygens.pergamon.resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/about")
 public class AboutResource {
   @GET
+  @Produces(MediaType.APPLICATION_JSON)
   public Response get() {
-    return Response.ok("about-info").build();
+    return Response.ok().entity(new AboutInfo("about")).build();
+  }
+
+  private class AboutInfo {
+    private final String msg;
+
+    AboutInfo(String msg) {
+      this.msg = msg;
+    }
+
+    public String getMsg() {
+      return msg;
+    }
   }
 }
